7520a3e2267fe9bec367ab9f17f07243ca617481
Alex Espinoza <aespinoza@aspria.net>
2014-01-20 11:54:37 -0500

    Video seems to be fine now

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   var videobackground = new $.backgroundVideo($('.video-container'), {
     "align": "centerXY",
-    "width": 1980,
-    "height": 1080,
+    "width": 1280,
+    "height": 720,
     "path": "assets/videos/",
     "filename": "croissant",
     "types": ["mp4","webm"]
